60a013944940aa3db85a2fc731bdbbb6352f4d65
ajscodes <ayushmaradia@gmail.com>
2025-11-18 13:20:47 +0530

    Remaining

--- SQL_Day_1_2.sql
+++ SQL_Day_1_2.sql
@@ -52,4 +52,98 @@ VALUES
 --DEPT:
 
 --PART-A:
+
 --1. Combine information from Person and Department table using cross join or Cartesian product.
+SELECT P.PersonName, D.DepartmentName FROM PERSON P, DEPT D
+SELECT P.PersonName, D.DepartmentName FROM PERSON P CROSS JOIN DEPT D
+
+--2. Find all persons with their department name.
+SELECT P.PersonName, D.DepartmentName FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID
+
+--3. Find all persons with their department name & code.
+SELECT P.PersonName, D.DepartmentName, D.DepartmentCode FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID
+
+--4. Find all persons with their department code and location.
+SELECT P.PersonName, D.DepartmentCode, D.Location FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID
+
+--5. Find the detail of the person who belongs to Mechanical department.
+SELECT P.PersonID, P.PersonName, P.DepartmentID, P.Salary, P.JoiningDate, P.City FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE D.DepartmentName = 'MECHANICAL'
+
+--6. Final person’s name, department code and salary who lives in Ahmedabad city.
+SELECT P.PersonName, D.DepartmentCode, P.Salary FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE P.City = 'AHMEDABAD'
+
+--7. Find the person's name whose department is in C-Block.
+SELECT P.PersonName FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE D.Location = 'C-BLOCK' 
+
+--8. Retrieve person name, salary & department name who belongs to Jamnagar city.
+SELECT P.PersonName, P.Salary, D.DepartmentName FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE P.City = 'JAMNAGAR'
+
+--9. Retrieve person’s detail who joined the Civil department after 1-Aug-2001.
+SELECT P.PersonID, P.PersonName, P.DepartmentID, P.Salary, P.JoiningDate, P.City FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE D.DepartmentName = 'CIVIL' AND P.JoiningDate > '1-AUG-2001'
+
+--10. Display all the person's name with the department whose joining date difference with the current date is more than 365 days.
+SELECT P.PersonName, D.DepartmentName FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE DATEDIFF(DAY,JoiningDate, GETDATE()) > 365
+
+--11. Find department wise person counts.
+SELECT D.DepartmentName, COUNT(P.PersonID) FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID GROUP BY D.DepartmentName
+
+--12. Give department wise maximum & minimum salary with department name.
+SELECT D.DepartmentName, MAX(P.Salary), MIN(P.Salary) FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID GROUP BY D.DepartmentName
+
+--13. Find city wise total, average, maximum and minimum salary. 
+SELECT P.City, SUM(P.Salary), AVG(P.Salary), MAX(P.Salary), MIN(P.Salary) FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID GROUP BY P.City
+
+--14. Find the average salary of a person who belongs to Ahmedabad city.
+SELECT AVG(P.Salary) FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE P.City = 'AHMEDABAD'
+
+--15. Produce Output Like: <PersonName> lives in <City> and works in <DepartmentName> Department.
+SELECT 
+	P.PersonName + 'LIVES IN' + P.City + 'AND WORKS IN' + D.DepartmentName
+FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID
+
+
+--PART-B:
+
+--1. Produce Output Like: <PersonName> earns <Salary> from <DepartmentName> department monthly.
+SELECT P.PersonName + ' EARN ' + CAST(P.Salary AS varchar(50)) + ' FROM ' + D.DepartmentName + ' DEPARTMENT MONTHLY.' FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID 
+
+--2. Find city & department wise total, average & maximum salaries.
+SELECT P.City, D.DepartmentName, SUM(P.Salary), AVG(P.Salary), MAX(P.Salary) FROM PERSON P JOIN DEPT D ON P.DepartmentID = D.DepartmentID GROUP BY P.City, D.DepartmentName
+
+--3. Find all persons who do not belong to any department.
+SELECT P.PersonID, P.PersonName, P.DepartmentID, P.Salary, P.JoiningDate, P.City FROM PERSON P LEFT JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE D.DepartmentName IS NULL
+--LEFT JOIN OR LEFT OUTER JOIN BOTH CORRECT (OUTER IS OPTIONAL)
+
+--4. Find all departments whose total salary is exceeding 100000.
+SELECT D.DepartmentName, SUM(P.Salary) FROM PERSON P RIGHT JOIN DEPT D ON P.DepartmentID = D.DepartmentID GROUP BY D.DepartmentName HAVING SUM(P.Salary) > 100000
+
+
+--PART-C:
+
+--1. List all departments who have no person.
+SELECT D.DepartmentName FROM PERSON P RIGHT JOIN DEPT D ON P.DepartmentID = D.DepartmentID WHERE P.PersonID IS NULL
+
+--2. List out department names in which more than two persons are working.
+SELECT DEPT.DepartmentName,COUNT(PERSON.PersonID) as PersonCount 
+FROM PERSON JOIN DEPT
+ON PERSON.DepartmentID=DEPT.DepartmentID
+GROUP BY DEPT.DepartmentName
+HAVING COUNT(PERSON.PersonID)>2
+
+--3. Give a 10% increment in the computer department employee’s salary.
+SELECT PERSON.PersonName,PERSON.Salary
+FROM PERSON JOIN DEPT
+ON PERSON.DepartmentID=DEPT.DepartmentID
+WHERE DEPT.DepartmentName='Computer'
+
+UPDATE PERSON
+SET Salary=SALARY+SALARY*0.1
+FROM PERSON JOIN DEPT
+ON PERSON.DepartmentID=DEPT.DepartmentID
+WHERE DEPT.DepartmentName='Computer'
+
+SELECT PERSON.PersonName,PERSON.Salary
+FROM PERSON JOIN DEPT
+ON PERSON.DepartmentID=DEPT.DepartmentID
+WHERE DEPT.DepartmentName='Computer'
+
